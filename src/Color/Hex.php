19093813d6383ef2774386b39674ba73124b5ab8
Martin <hello@northrook.com>
2024-03-27 13:20:43 +0000

    mend

---
+++ src/Color/Hex.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Northrook\Types\Color;
+
+use Northrook\Logger\Log;
+use Northrook\Types\Interfaces\Printable;
+use Northrook\Types\Type\Type;
+use Stringable;
+
+class Hex extends Type implements Printable, Stringable
+{
+
+    public function __construct(
+        string $color,
+    ) {
+        $this->value = strtolower( $color );
+
+        if ( $this->value[ 0 ] !== '#' ) {
+            $this->value = '#' . $this->value;
+        }
+
+        if ( strlen( $this->value ) !== 7 ) {
+            Log::Error(
+                'The type {class} was created, but did not pass validation.',
+                [
+                    `class` => `Color\Hex`,
+                    'value' => $this->value,
+                    'type'  => $this,
+                ],
+            );
+        }
+
+        parent::__construct();
+    }
+
+    public function print() : string {
+        return $this->value;
+    }
+
+    public function __toString() : string {
+        return $this->print();
+    }
+}
